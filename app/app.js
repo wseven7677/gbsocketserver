d70b98f474c6c0f70b8ba629e77259d0403e565e
tkyApril <caoyu_92@126.com>
2018-07-21 14:22:49 +0000

    socket io init

--- app/app.js
+++ app/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
+import socket from 'socket.io'
+import http from 'http'
 
 import bodyparser from 'body-parser'
 import multer from 'multer'
 
 var app = express(),
+    httpser = http.Server(app),
+    io = socket(httpser),
+
     nodePort = 8010,
-    rs = ['history', 'pictures', 'members','news'], // 需手动添加数据库中的collcetions，后续改自动识别。
+    
     storage = multer.diskStorage({
         destination: '/usr/cycodes/greenbluechatserver/files',
         filename(req, file, cb) {
@@ -21,41 +26,36 @@ app.use(bodyparser.urlencoded({
 }));
 app.use(bodyparser.json());
 
-// === GET & cardCollection post ===
+/**********************************/
 
-    // ---get:---
-    app.get('/api/' + value, function(req, res) {
+    app.get('/io', function(req, res) {
 
-        // 后端验证：
-
-        // 取数据，发数据：
-        mongo(value, req.query, function(resd) {
-            res.send(resd);
-        });
+            res.sendFile(__dirname + '/index.html');
     });
 
 // ---upload pictures---
-
 app.post('/api/members2uploadpic/', upload.single('imgObj'), function(req, res) {
     // 取数据，发数据：
-    mongo('members', handleUploadPic(req), function(resd) {
+ /*   mongo('members', handleUploadPic(req), function(resd) {
         res.send(resd);
-    });
+    });*/
 });
 
 // ----- user validate ------
 
 app.post('/api/users', (req, res) => {
-    // 取数据，发数据：
-    mongoUser('users', req.body, resd => {
+  /*  mongoUser('users', req.body, resd => {
         res.send(resd); // true
-    });
+    });*/
 });
 
-
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 
 // =============================
 app.listen(nodePort, 'localhost', function() {
     console.log('service is on.');
 });
+
