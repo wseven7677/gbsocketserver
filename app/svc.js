236c34964f269e2b3f88510df9439493ddebe286
caoyu13 <caoyu13@bd.com>
2018-07-23 19:39:11 +0800

    3

--- app/svc.js
+++ app/svc.js
@@ -7,37 +7,38 @@ let mongoClient = mongodb.MongoClient,
 
 const getRoleName = function(callback) {
     mongoClient.connect(dbUrl, function(err, db) {
-            if (err) {
-                throw err;
-            }
-            console.log('database connected.');
-
-            var onedb = db.db(dbName),
-                oneCollection = onedb.collection('availableRoleNameList'),
-                gotName;
-            // 随机取名字：
-            oneCollection.find().toArray(function(err2, availableList) {
-                if (err2) {
-                    throw err2;
-                }
-
-                var len = availableList.length,
-                    num = Math.round(Math.random() * (len - 1));
-
-                gotName = availableList[num];
-
-            });
-            // 将该名字从该集合去除
-            oneCollection.remove({
-                name: gotName
-            }, {
-                justOne: true
-            });
-            // 返回数据处:
-            callback(gotName);
+        if (err) {
+            throw err;
         }
+        console.log('database connected.');
 
-        db.close(); console.log('database closed.');
+        var onedb = db.db(dbName),
+            oneCollection = onedb.collection('availableRoleNameList'),
+            gotName;
+        // 随机取名字：
+        oneCollection.find().toArray(function(err2, availableList) {
+            if (err2) {
+                throw err2;
+            }
+
+            var len = availableList.length,
+                num = Math.round(Math.random() * (len - 1));
+
+            gotName = availableList[num];
+
+        });
+        // 将该名字从该集合去除
+        oneCollection.remove({
+            name: gotName
+        }, {
+            justOne: true
+        });
+        // 返回数据处:
+        callback(gotName);
+
+        db.close();
+        console.log('database closed.');
+
     });
 };
 
