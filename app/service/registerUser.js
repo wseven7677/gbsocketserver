a943ba271f6966634204fb13be1fefcb219e7bda
caoyu13 <caoyu13@bd.com>
2018-12-12 00:43:46 +0800

    fix

--- app/service/registerUser.js
+++ app/service/registerUser.js
@@ -17,24 +17,40 @@ const registerUser = function (req, callback) {
             oneCollection = onedb.collection('userIds'),
             colMapScores = onedb.collection('mapScores');
 
-        oneCollection.insert({
-            uid: req.uid,
-            pw: req.pw,
-            name: req.name
-        });
-        colMapScores.insert({
-            uid: req.uid,
-            score: 0
-        });
+            oneCollection.find().toArray(function(err2, idList) {
+                if (err2) {
+                    throw err2;
+                }
+    
+                let list = idList.map(one => {
+                    return one.uid;
+                });
 
-        callback({
-            code: 1,
-            msg: 'done.'
-        });
-
-        db.close();
-        console.log('database closed for registerUser');
-
+                if(list.indexOf(req.uid) > -1) {
+                    callback({
+                        code: 0,
+                        msg: '用户已存在'
+                    });
+                }else {
+                    oneCollection.insert({
+                        uid: req.uid,
+                        pw: req.pw,
+                        name: req.name
+                    });
+                    colMapScores.insert({
+                        uid: req.uid,
+                        score: 0
+                    });
+            
+                    callback({
+                        code: 1,
+                        msg: 'done.'
+                    });
+                }
+    
+                db.close();
+                console.log('database closed for userLogin');
+            });
 
     });
 };
